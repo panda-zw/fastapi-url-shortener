3cb40cb28025b58e583269b85ceac37c6d13dfd1
Panashe Mapika <mapikapnsh@gmail.com>
2024-07-24 20:29:25 +0200

    add date to url created

--- models/url.py
+++ models/url.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel, HttpUrl
+from datetime import datetime
 
 
 class URL(BaseModel):
     original_url: str
     short_url: str
+    created_at: datetime
 
 
 class UrlRequest(BaseModel):
